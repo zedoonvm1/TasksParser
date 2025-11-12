0db88e4c18f0fdfe6e59c2b5736a5a6256e51250
zedoon <45298882+zedoonvm1@users.noreply.github.com>
2025-11-13 02:43:24 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string message = "This file is here for GitHub to mark the repository as C++";
+    std::cout << message << std::endl;
+    return 0;
+}
